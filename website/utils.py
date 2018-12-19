5f48da3a02769b39513f2c1bc95395b0113d02ae
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-12-19 23:34:29 +0800

    Templates for result cards is added. Landing page templates and inital form data are updated. Pagination setting is added.

--- website/utils.py
+++ website/utils.py
@@ -8,6 +8,18 @@ DISPLAY_UTILS = {
 FORM_INIT = {
     'searchType': 'all',
     'query': '',
+    'image': '',
+}
+
+PAGI_INIT = {
+    'offset': 3,
+    'prev': '前一页',
+    'next': '后一页',
+    'top': '首页',
+    'bottom': '尾页',
+    'cur_page': 1,
+    'max_page': 10,
+    'result_per_page': 9,
 }
 
 LANDING_DATA_DEFAULT = {
@@ -53,27 +65,128 @@ LANDING_DATA_DEFAULT = {
     'random_ancient': [
         {
             'title': 'Title',
-            'content': '我是一首现代诗 我是一首现代诗 我是一首现代诗 我是一首现代诗 ',
+            'content': '我是一首古诗 我是一首古诗 我是一首古诗 我是一首古诗 ',
             'poet': 'Someone famous in My memories',
             'poemurl': '/index',
             'poeturl': '/index',
         },
         {
             'title': 'Title',
-            'content': '我是一首现代诗 我是一首现代诗 我是一首现代诗 我是一首现代诗',
+            'content': '我是一首古诗 我是一首古诗 我是一首古诗 我是一首古诗',
             'poet': 'Someone famous in My memories',
             'poemurl': '/index',
             'poeturl': '/index',
         },
         {
             'title': 'Title',
-            'content': '我是一首现代诗 我是一首现代诗 我是一首现代诗 我是一首现代诗',
+            'content': '我是一首古诗 我是一首古诗 我是一首古诗 我是一首古诗',
             'poet': 'Someone famous in My memories',
             'poemurl': '/index',
             'poeturl': '/index',
         },
     ],
+    'daily_ancient': {
+        'title': 'Title',
+        'content': "这是一首每日推荐 这是一首每日推荐 这是一首每日推荐 这是一首每日推荐",
+        'poet': 'Someone famous in My memories',
+        'poemurl': '/index',
+        'poeturl': '/index',
+    },
+    'labels': [
+        {
+            'label': '标签标签标签',
+            'labelurl': '#',
+        },
+        {
+            'label': '标签',
+            'labelurl': '#',
+        },
+        {
+            'label': '标签标签',
+            'labelurl': '#',
+        },
+        {
+            'label': '标签',
+            'labelurl': '#',
+        },
+        {
+            'label': '标签标签',
+            'labelurl': '#',
+        },
+    ],
 }
+
+ENTRY_SAMPLES = [
+    {
+        'imgurl': '/static/image/1.jpg',
+        'title': 'Title',
+        'content': '我是一首诗 我是一首诗 我是一首诗 我是一首诗',
+        'poet': '诗人',
+        'poemurl': '#',
+        'poeturl': '#',
+        'labels': [
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+        ],
+        'likes': 321,
+    },
+    {
+        'imgurl': '/static/image/1.jpg',
+        'title': 'Title',
+        'content': '我是一首诗 我是一首诗 我是一首诗 我是一首诗',
+        'poet': '诗人',
+        'poemurl': '#',
+        'poeturl': '#',
+        'labels': [
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+        ],
+        'likes': 321,
+    },
+    {
+        'imgurl': '/static/image/1.jpg',
+        'title': 'Title',
+        'content': '我是一首诗 我是一首诗 我是一首诗 我是一首诗',
+        'poet': '诗人',
+        'poemurl': '#',
+        'poeturl': '#',
+        'labels': [
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+            {
+                'label': '标签',
+                'labelurl': '#',
+            },
+        ],
+        'likes': 321,
+    },
+]
 
 with codecs.open('templates/header.html', 'r', encoding='utf-8') as f:
     HEADER = ''.join(f.readlines())
