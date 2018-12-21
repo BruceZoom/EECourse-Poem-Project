65f021815b9172f5b7c80580a66ca71fe49cba8c
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-12-21 21:55:58 +0800

    image crawler update

--- image_crawler/image_crawler.py
+++ image_crawler/image_crawler.py
@@ -22,11 +22,15 @@ if __name__ == '__main__':
 		f = int(sys.argv[1])
 	if len(sys.argv) > 2:
 		t = int(sys.argv[2])
+	s = 1
+	if f > t:
+		s = -1
 
 	if not os.path.exists("veer/fengguang/"):
 		os.mkdirs("veer/fengguang/")
 	driver = webdriver.Firefox()
-	for i in range(f, t+1):
+	for i in range(f, t, s):
+		print 'current page:', i
 		url = "https://www.veer.com/query/photo?phrase=%E9%A3%8E%E5%85%89&page={}&perpage=200".format(i)
 		filename = "veer/fengguang/page-{}.html".format(i)
 		save_page(driver, url, filename)
