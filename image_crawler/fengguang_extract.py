9ad79aea2c17d02ffaf43d2ee77f9f4eeebc2984
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-12-21 21:41:32 +0800

    Image crawler for veer is added.

---
+++ image_crawler/fengguang_extract.py
@@ -0,0 +1,30 @@
+# coding = utf-8
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import urllib2
+import urlparse
+import json
+import codecs
+import sys
+import os
+
+def save_page(driver, url, filename):
+	driver.get(url)
+	html = driver.execute_script("return document.documentElement.outerHTML")
+	with codecs.open(filename, 'w', encoding='utf-8') as fout:
+		fout.write(html)
+
+if __name__ == '__main__':
+	f = 1
+	t = (4531586 + 199) // 200
+	if len(sys.argv) > 1:
+		f = sys.argv[2]
+	if len(sys.argv) > 2:
+		t = sys.argv[3]
+
+	driver = webdriver.Firefox()
+	for i in range(f, t+1):
+		url = "https://www.veer.com/query/photo?phrase=%E9%A3%8E%E5%85%89&page={}&perpage=200".format(i)
+		filename = "veer/fengguang/page-{}.html".format(i)
+		save_page(driver, url, filename)
