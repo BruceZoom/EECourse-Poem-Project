eb366e5d81a2aaa65fe1c0eab369c19f423fb39e
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-12-26 16:06:02 +0800

    back-end changes

--- website/PoemModel.py
+++ website/PoemModel.py
@@ -1,12 +1,14 @@
 # coding:utf-8
 import lucene
-from Index.modern_search import ModernPoemSearch
+from Index.modern_search import ModernPoemSearcher
+from Index.ancient_search import AncientPoemSearcher
 import utils
 
 vm_env = lucene.initVM(vmargs=['-Djava.awt.headless=true'])
 print 'vm initialized'
 
-MPS = ModernPoemSearch('Index/modern_index')
+MPS = ModernPoemSearcher('Index/modern_index')
+APS = AncientPoemSearcher('Index/gushiwen_index')
 
 """
 command_dict:
@@ -15,10 +17,10 @@ command_dict:
 """
 
 
-def common_query(input_dict, cur_page):
-    if input_dict['searchType'] == "gushiwen":
+def common_query(input_dict, cur_page=1):
+    if input_dict['searchType'] == "ancient":
         return shi_search(input_dict, cur_page)
-    elif input_dict['searchType'] == "xiandaishi":
+    elif input_dict['searchType'] == "modern":
         return modern_search(input_dict, cur_page)
     elif input_dict['searchType'] == "all":
         return mixed_search(input_dict, cur_page)
@@ -26,11 +28,13 @@ def common_query(input_dict, cur_page):
         raise ValueError("Undefined Search Type")
 
 
-def modern_search(input_dict, cur_page):
+def modern_search(input_dict, cur_page=1):
     pass
 
-def shi_search(input_dict, cur_page):
+
+def shi_search(input_dict, cur_page=1):
     pass
 
-def mixed_search(input_dict, cur_page):
+
+def mixed_search(input_dict, cur_page=1):
     pass
