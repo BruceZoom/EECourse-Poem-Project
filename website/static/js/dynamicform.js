c4bf58a7139b476d68b192e636288dd1f4972497
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2018-12-26 22:42:44 +0800

    Optimized header file. Slide button is added.

--- website/static/js/dynamicform.js
+++ website/static/js/dynamicform.js
@@ -1,1 +1,17 @@
-jQuery(document).ready()
+// jQuery("#accurate").change(function () {
+//     console.log(jQuery("#accurate").prop("checked"));
+//     if(jQuery("#accurate").prop("checked")){
+//         if(jQuery("#accurate").val() === "ancient"){
+//             jQuery("#ancient-search-advanced").collapse("show");
+//             jQuery("#modern-search-advanced").collapse("hide");
+//         }
+//         else{
+//             jQuery("#ancient-search-advanced").collapse("hide");
+//             jQuery("#modern-search-advanced").collapse("show");
+//         }
+//     }
+//     else{
+//         jQuery("#ancient-search-advanced").collapse("hide");
+//         jQuery("#modern-search-advanced").collapse("hide");
+//     }
+// });
